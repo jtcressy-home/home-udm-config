6bb6c3fe0f6873212af7273d7d159458c5c3d0c3
Joel Cressy <joel.cressy@dottid.com>
2022-08-29 15:22:11 -0500

    fix tailscale paths

--- static/scripts/ip-rule-monitor.sh
+++ static/scripts/ip-rule-monitor.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 RULE_PRIORITY="5225"
+INTERVAL="60s"
 
 function getDefaultTable() {
         /sbin/ip rule list priority 32766 | cut -d " " -f 4
@@ -13,11 +14,18 @@ function updateTailscaleRule() {
 echo Routing table with default route is $(getDefaultTable)
 updateTailscaleRule $(getDefaultTable)
 
-tail -Fn 0  /var/log/messages | while read line; do
-        table=`echo $line | grep -e "ubios-udapi-server: wanFailover"`
-        if [[ "$table" != ""  ]]
-        then
-                echo Detected WAN failover: Routing table with default route is $(getDefaultTable) now, adjusting rule >> /var/log/messages
-                updateTailscaleRule $(getDefaultTable)
-        fi
+until false; do
+  echo Routing table with default route is $(getDefaultTable), updating rule $RULE_PRIORITY
+  updateTailscaleRule $(getDefaultTable)
+  echo Checking again in $INTERVAL
+  sleep $INTERVAL
 done
+
+#tail -Fn 0  /var/log/messages | while read line; do
+#        table=`echo $line | grep -e "ubios-udapi-server: wanFailover"`
+#        if [[ "$table" != ""  ]]
+#        then
+#                echo Detected WAN failover: Routing table with default route is $(getDefaultTable) now, adjusting rule >> /var/log/messages
+#                updateTailscaleRule $(getDefaultTable)
+#        fi
+#done
