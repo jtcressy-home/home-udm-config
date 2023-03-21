17eb7d469597617b22a13e183c2d757667231998
Joel Cressy <joel.cressy@dottid.com>
2023-03-21 15:33:18 -0500

    refactor entire repo for Unifi OS 2.x+

--- locals.tf
+++ locals.tf
@@ -1,10 +1,6 @@
 locals {
-  persistent_storage_dir = "/mnt/data"
-  external_storage_dir   = "/mnt/data_ext"
-  on_boot_dir            = "${local.persistent_storage_dir}/on_boot.d"
-  systemd_unit_dir       = "${local.persistent_storage_dir}/etc/systemd/system"
-  systemd_config_dir     = "${local.persistent_storage_dir}/etc/"
-  systemd_data_dir       = "${local.persistent_storage_dir}/var/"
+  persistent_storage_dir = "/persistent"
+  external_storage_dir   = "/volume1"
   udm_network_cidrs = toset([
     "192.168.20.0/24",
     "192.168.22.0/24",
